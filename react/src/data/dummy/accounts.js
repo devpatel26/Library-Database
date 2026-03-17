573bc651bec89c38a3b72e8144d950d7138c5ff8
Rainer <136118910+RhythmNotebook@users.noreply.github.com>
2026-03-17 14:50:33 -0500

    More dummy data

---
+++ react/src/data/dummy/accounts.js
@@ -0,0 +1,54 @@
+export const patronAccounts = [
+  {
+    id: 1231653543,
+    role: "student",
+    firstname: "John",
+    lastname: "Doe",
+    dateofbirth: "10-24-1990",
+    email: "testemail1@email.com",
+  },
+  {
+    id: 54353134,
+    role: "student",
+    firstname: "Jane",
+    lastname: "Doe",
+    dateofbirth: "03-24-2000",
+    email: "testemail2@email.com",
+  },
+  {
+    id: 4356386747,
+    role: "professor",
+    firstname: "Jones",
+    lastname: "Dane",
+    dateofbirth: "05-20-1980",
+    email: "testemail3@email.com",
+  },
+];
+export const staffAccounts = [
+  {
+    id: 1231653543,
+    role: "staff",
+    firstname: "Jane",
+    lastname: "Dane",
+    dateofbirth: "02-04-2001",
+    email: "testemail4@email.com",
+  },
+  {
+    id: 54353134,
+    role: "staff",
+    firstname: "Jenny",
+    lastname: "Doe",
+    dateofbirth: "07-20-1994",
+    email: "testemail5@email.com",
+  },
+  {
+    id: 4356386747,
+    role: "admin",
+    firstname: "Jone",
+    lastname: "Done",
+    dateofbirth: "05-01-1998",
+    email: "testemail6@email.com",
+  },
+];
+
+export default patronAccounts;
